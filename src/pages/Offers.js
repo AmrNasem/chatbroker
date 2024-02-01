17b2f2f6b6b81460407eb23f3c008464854462af
Mohammed Heggy <hegy395@gmail.com>
2024-02-01 20:53:31 +0200

    offers page

---
+++ src/pages/Offers.js
@@ -0,0 +1,18 @@
+import ProductItem from "../components/Home/ProductItem"
+import offers from "./Offers.module.css"
+
+export const Offers = () => {
+  return (
+    <>
+      <p className={offers.title}>العروض</p>
+      <div className={offers.offersContainer} >
+        {[...Array(15)].map((product, index) => (
+          <div className={offers.card}>
+            <ProductItem key={index} />
+          </div>
+        )
+        )}
+      </div>
+    </>
+  )
+}
