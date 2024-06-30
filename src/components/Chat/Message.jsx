1fc2fd96e6c6ee4df4b29b7c7e07c87cd1e9ecbc
Mohammed Heggy <hegy395@gmail.com>
2024-06-30 07:38:10 +0300

    handle the send time

--- src/components/Chat/Message.jsx
+++ src/components/Chat/Message.jsx
@@ -1,19 +1,28 @@
 import { useSelector } from "react-redux";
 import { getDate } from "../../utils/date";
+import { useEffect, useState } from "react";
 
 const Message = ({ message }) => {
   const user = useSelector((state) => state.auth.user);
   const isMyMessage = message.senderId === user.id;
 
+  const currentTime = new Date();
+
+  const formatTime = (date) => {
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
+    return `${hours}:${minutes}:${seconds}`;
+  };
   return (
     <div
       style={{ fontSize: "0.9rem" }}
-      className={`fw-semibold d-flex flex-column align-items-${
-        isMyMessage ? "start" : "end"
-      }`}
+      className={`fw-semibold d-flex flex-column align-items-${isMyMessage ? "start" : "end"
+        }`}
     >
       <span dir="ltr" className={`d-block mb-1 text-secondary`}>
-        {getDate(message.createdAt)}
+        {/* {getDate(message.createdAt)} */}
+        {formatTime(currentTime)}
       </span>
       <p
         className={`text-white p-2 ${isMyMessage ? "bg-sec" : "bg-main"}`}
